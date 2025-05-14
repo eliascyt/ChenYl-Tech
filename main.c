d3f44d02aefbcb33d114576a66d1fcf3a1bf79cd
eliascyt <boujnahelias@gmail.com>
2025-05-14 17:09:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "gestion_animaux.h"
 
 int main() {
-    printf("Bienvenue sur ChenYl-Tech !\n");
+    int choix;
+
+    do {
+        printf("\n=== MENU CHENYL-TECH ===\n");
+        printf("1. Ajouter un animal\n");
+        printf("2. Rechercher un animal\n");
+        printf("3. Supprimer un animal\n");
+        printf("4. Lister tous les animaux\n");
+        printf("0. Quitter\n");
+        printf("Choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                ajouter_animal();
+                break;
+            case 2:
+                rechercher_animaux();
+                break;
+            case 3:
+                supprimer_animal();
+                break;
+            case 4:
+                lister_animaux();
+                break;
+            case 0:
+                printf("Au revoir !\n");
+                break;
+            default:
+                printf("Choix invalide.\n");
+        }
+
+    } while (choix != 0);
+
     return 0;
 }
