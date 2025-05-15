9608865ba1a05c1b902c260211aa730fdf36c82d
eliascyt <boujnahelias@gmail.com>
2025-05-15 05:26:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "gestion_animaux.h"
+#include <stdio.h>  
+#include <stdlib.h> (malloc, free, etc.)
+#include "gestion_animaux.h" // Inclusion du fichier contenant les déclarations des fonctions liées à la gestion des animaux
 
 int main() {
-    int choix;
+    int choix; // Déclaration d'une variable pour stocker le choix de l'utilisateur dans le menu
 
     do {
+        // Affichage du menu principal
         printf("\n=== MENU CHENYL-TECH ===\n");
         printf("1. Ajouter un animal\n");
         printf("2. Rechercher un animal\n");
@@ -13,29 +14,29 @@ int main() {
         printf("4. Lister tous les animaux\n");
         printf("0. Quitter\n");
         printf("Choix : ");
-        scanf("%d", &choix);
+        scanf("%d", &choix); // Lecture du choix de l'utilisateur
 
+        // Traitement du choix de l'utilisateur
         switch (choix) {
             case 1:
-                ajouter_animal();
+                ajouter_animal(); // Appel de la fonction pour ajouter un nouvel animal
                 break;
             case 2:
-                rechercher_animaux();
+                rechercher_animal(); // Appel de la fonction pour rechercher un animal existant
                 break;
             case 3:
-                supprimer_animal();
+                supprimer_animal(); // Appel de la fonction pour supprimer un animal
                 break;
             case 4:
-                lister_animaux();
+                lister_animaux(); // Appel de la fonction pour afficher la liste de tous les animaux
                 break;
             case 0:
-                printf("Au revoir !\n");
+                printf("Au revoir !\n"); // Message de sortie du programme
                 break;
             default:
-                printf("Choix invalide.\n");
+                printf("Choix invalide. Veuillez réessayer.\n"); // Message d'erreur pour un choix non valide
         }
+    } while (choix != 0); // Boucle continue jusqu'à ce que l'utilisateur choisisse de quitter (choix == 0)
 
-    } while (choix != 0);
-
-    return 0;
+    return 0; // Fin du programme
 }
